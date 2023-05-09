7dee80a2a5c73e92667cb6e0a9cb0b9248fa7901
Erwin Mesias <mesiaserwin@gmail.com>
2023-05-10 05:51:17 +0800

    refactor: add renderSkills function

--- src/components/home/HomeHeader.tsx
+++ src/components/home/HomeHeader.tsx
@@ -1,28 +1,52 @@
+import { useCallback } from 'react';
 import { ExternalLink } from 'react-feather';
 import { URL_UPWORK } from '@/config/site';
 import Image from 'next/image';
 import image from '@/assets/images/mesi.webp';
-import Pipe from './SkillPipe';
+import Pipe from '@/components/home/SkillPipe';
+
+const majorTools = [
+  'PHP',
+  'Laravel',
+  'Vue.js',
+  'Nuxt.js',
+  'React.js',
+  'Next.js',
+];
 
 function HomeHeader() {
+  const renderSkills = useCallback(
+    () =>
+      majorTools.map((item, index) => (
+        <>
+          {item} {index < majorTools.length - 1 && <Pipe />}
+        </>
+      )),
+    []
+  );
+
   return (
     <section className="flex gap-y-6 sm:gap-x-8 items-center my-12 flex-col sm:flex-row max-w-2xl mx-auto">
       <span>
-        <Image src={image} width="128" height="128" alt="Erwin Mesias" />
+        <Image
+          src={image}
+          width="128"
+          height="128"
+          alt="Erwin Mesias"
+          loading="eager"
+        />
       </span>
       <div className="space-y-1">
         <h1 className="text-center sm:text-left text-2xl">Erwin Mesias</h1>
-        <h2 className="text-center sm:text-left text-grey-200 text-base">
+        <p className="text-center sm:text-left text-grey-200 text-base">
           Full-Stack Web & Mobile Developer
-        </h2>
-        <h3 className="text-center sm:text-left text-grey-300 hover:text-grey-300 hover:underline flex items-center justify-center sm:justify-start">
+        </p>
+        <p className="text-center sm:text-left text-grey-300 hover:text-grey-300 hover:underline flex items-center justify-center sm:justify-start">
           <a href={URL_UPWORK} target="_blank">
             Upwork Freelancer <ExternalLink className="ml-1 inline" size="14" />
           </a>
-        </h3>
-        <p className="text-center sm:text-left text-sm">
-          PHP <Pipe /> Laravel <Pipe /> Vue.js <Pipe /> Nuxt.js <Pipe /> React.js <Pipe /> Next.js
         </p>
+        <p className="text-center sm:text-left text-sm">{renderSkills()}</p>
       </div>
     </section>
   );
--- src/components/home/SkillPipe.tsx
+++ src/components/home/SkillPipe.tsx
@@ -1,5 +1,5 @@
 function SkillPipe() {
-  return <span className="inline-block text-grey-700 mx-2">|</span>;
+  return <span className="inline-block text-grey-700 mx-1.5">|</span>;
 }
 
 export default SkillPipe;
